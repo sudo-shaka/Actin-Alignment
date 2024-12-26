51e4b4ec5419d9b037d1da61ee3310dba476cb48
sudo-shaka <68864205+sudo-shaka@users.noreply.github.com>
2024-12-26 14:47:43 -0500

    Update imgRead.hpp

--- include/imgRead.hpp
+++ include/imgRead.hpp
@@ -1,5 +1,5 @@
-#include <vector>
+#include <filesystem>
 #include <opencv2/opencv.hpp>
 
 std::vector<std::filesystem::path> imgPaths(std::vector<std::string> arg);
-std::vector<std::vector<int>> im2arr(cv::Mat image);
+int** im2arr(cv::Mat image);
