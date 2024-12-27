2df25d72ac2869578703d48767610934ad3ce2e6
sudo-shaka <68864205+sudo-shaka@users.noreply.github.com>
2024-12-26 20:01:10 -0500

    Added csvHandler.hpp

--- include/csvHandler.hpp
+++ include/csvHandler.hpp
@@ -1,6 +1,5 @@
 #include <vector>
 #include <map>
 #include <string>
-
 void csvInit(std::string file, std::vector<float> angles);
 void csvAppendHist(std::string file, std::string conditionName,std::map<float,int> hist);
